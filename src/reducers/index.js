01bad7ec24bd74cc2e963005219ff52dd9aef4d2
sachinkondana20 <sachin.k@codecraft.co.in>
2020-02-05 15:35:37 +0530

    feat(redux): Integrate redux into the application

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import setups from './setups';
+
+export default combineReducers({
+    setups,
+});
---
+++ src/reducers/setups.js
@@ -0,0 +1,12 @@
+const initialState = {
+  appVersion: '0.0.1'
+};
+
+const todos = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return initialState
+  }
+};
+
+export default todos;
